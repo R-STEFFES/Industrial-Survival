037e5491b49431ca832e8771cc3c9231106df2fb
R_STEFFES <raphael.steffes@gmail.com>
2026-05-30 20:22:17 +0200

    more sub modules

--- mods/xcompat/src/stairs.lua
+++
@@ -1,8 +0,0 @@
-local filename = xcompat.gameid
-
---if we dont have a stairs file for the game, use xcompat_agnostic
-if not xcompat.utilities.file_exists(xcompat.modpath .. "/src/stairs/" .. filename .. ".lua") then
-    filename = "xcompat_agnostic"
-end
-
-return dofile(xcompat.modpath .. "/src/stairs/" .. filename .. ".lua")
